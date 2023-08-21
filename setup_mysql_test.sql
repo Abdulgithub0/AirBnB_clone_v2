79e0f9c0cc872e0eaa1ebf93c3168ca55d456013
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-08-21 12:17:44 +0100

    setup script for mysql server for airbnb test database

---
+++ setup_mysql_test.sql
@@ -0,0 +1,7 @@
+-- A script that create a test db on Mysql server
+-- also create test username with grant to the test db
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
