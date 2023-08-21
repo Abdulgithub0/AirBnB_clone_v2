6297afdb5af589a2a3ce748f616dc9db397181b6
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-08-21 11:59:49 +0100

    setup script for mysql development server

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,6 @@
 -- a script that prepares a MySQL server for this project
 -- a development database called hbnb_dev_db will be used
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE DATABASE IF NOT EXISTS performance_schema;
 CREATE USER IF NOT EXISTS "hbnb_dev"@"localhost" IDENTIFIED BY "hbnb_dev_pwd";
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO "hbnb_dev"@"localhost";
 GRANT SELECT ON performance_schema.* TO "hbnb_dev"@"localhost";
