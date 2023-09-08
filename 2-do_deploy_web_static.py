4f7d3900ddce4a353363c9e510ad0f8239ea163e
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-08 10:40:17 +0100

    ship static files to server

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-a Fabric script (based on the file 1-pack_web_static.py)
+A Fabric script (based on the file 2-do_deploy_web_static.py)
 that distributes an archive to your web servers
 """
 from fabric.api import *
@@ -8,20 +8,26 @@ env.hosts = ["35.153.18.178", "52.201.158.90"]
 
 
 def do_deploy(archive_path):
-    """deploy web_static files"""
+    """Deploy web_static files"""
     try:
         if archive_path:
-            file_name = archive_path[9:]
+            # Extract the filename from the path
+            file_name = archive_path.split("/")[-1]
             sudo("mkdir -p /tmp/")
             put(archive_path, "/tmp/")
             releases = "/data/web_static/releases/" + file_name[:-4]
             sudo("mkdir -p {}".format(releases))
             sudo("tar -xzvf /tmp/{} -C {}/".format(file_name, releases))
             sudo("rm /tmp/{}".format(file_name))
-            if run("test -h /data/web_static/current").succeeded:
-                sudo("rm -rf /data/web_static/current")
-            sudo("mkdir -p /data/web_static/current")
-            sudo("ln -sf {} /data/web_static/current".format(releases))
+            # relocate files location to allow easy aliasing by nginx
+            sudo("mv {}/web_static/* {}".format(releases, releases))
+            sudo("rm -rf {}/web_static".format(releases))
+            # Check if the symbolic link exists before removing it
+            sudo("rm -rf /data/web_static/current")
+            sudo("mkdir -p /data/web_static && touch /data/web_static/current")
+            # Create a new symbolic link
+            sudo("ln -sf {}/ /data/web_static/current".format(releases))
+            print("...All the Files have been deployed successfully...")
             return True
     except Exception as e:
         return False
