1586e6e92087f0c03d34b7a87b0743f62f829137
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-23 15:44:42 +0100

    a script that starts a Flask web application

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-""" a script that starts a Flask web application"""
+"""
+a script that starts a Flask web application:
+the web application is listening  on 0.0.0.0, port 5000
+Routes:
+    /: display “Hello HBNB!”
+"""
 from flask import Flask
 
 # create an instance of class Flask
@@ -9,9 +14,10 @@ app = Flask(__name__)
 # bind a specific url to the view hello function
 @app.route("/", strict_slashes=False)
 def hello():
+    """Return an html page with body containing hello HBNB"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
     # launch the hello_route app
-    app.run(host=0, port=5000)
+    app.run(host=0)
