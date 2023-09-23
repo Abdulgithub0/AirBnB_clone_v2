a67528b2586949f92946e261a4fa303aafa84f11
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-23 16:15:36 +0100

    a script that starts a Flask web application

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -5,6 +5,8 @@ the web application is listening  on 0.0.0.0, port 5000
 Routes:
     /: display “Hello HBNB!”
     /hbnb: display "HBNB"
+    /c/<text>: display “C ” followed by the value of the text
+        variable (replace underscore _ symbols with a space)
 """
 from flask import Flask
 
@@ -12,15 +14,21 @@ from flask import Flask
 app = Flask(__name__)
 
 
-# bind a specific url to the views: hello and hello_root
+# bind a specific different url to the following views
+@app.route("/c/<text>", strict_slashes=False)
+def c_view():
+    """display 'C' followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
 @app.route("/hbnb", strict_slashes=False)
-def hello():
+def hbnb_view():
     """Return an html page with body containing HBNB"""
     return "HBNB"
 
 
 @app.route("/", strict_slashes=False)
-def hello_root():
+def default_view():
     """Return an html page with body containing hello HBNB"""
     return "Hello HBNB!"
 
