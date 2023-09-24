d9b9f67b6399ca8a1e76e1bbc5a44b212f44af60
Abdulgithub0 <belloabdulsamad26@gmail.com>
2023-09-24 17:12:15 +0100

    display all states or filter by id

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -14,20 +14,28 @@ app = Flask(__name__)
 @app.teardown_appcontext
 def close_session(error=None):
     """close the current open storage session for each request made"""
-    # print("..........connection close...............")
     storage.close()
 
 
 # implement the logic for state listings
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states_view():
+@app.route("/states", strict_slashes=False)
+@app.route("/states/<id>", strict_slashes=False)
+def states_view(id=None):
     """
-        list or display all instances of City obj with
-        corresponding State obj in either of the specify storage engine
+        /states
+            list or display all instances of State obj with
+            in either of the specify storage engine
+
+        /states/<id>
+            list or display all instances of State with respective cities
+            obj based on search id parameter
     """
     from models.state import State
     states = storage.all(State)
-    return render_template("8-cities_by_states.html", States=states)
+    if (id):
+        states = dict(filter(lambda x: x[1].id == id, states.items())) 
+    return render_template("8-cities_by_states.html", States=states, search_id=id)
+
 
 
 if __name__ == "__main__":
